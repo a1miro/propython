49d4dd063b074ad415f3eada995f45f09ce8c2a8
Andrei Mironenko <andrei.mironenko@gmail.com>
2018-08-12 00:10:43 +0100

    Decorators with adjustable attributes

--- my-experiements/decorators/decorator_w_adjustable_attributes.py
+++ my-experiements/decorators/decorator_w_adjustable_attributes.py
@@ -3,6 +3,23 @@ Created on 10 Aug 2018
 
 @author: amironenko
 '''
-#p.354
+from functools import wraps, partial
+import logging
+
+def add_attribute(attr, value):
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            setattr(func, attr, value)
+            return func(*args, **kwargs)
+        return inner
+    return wrapper
+    
+@add_attribute("unittest", True)
+def a():    
+   pass 
+
+
+
 if __name__ == '__main__':
-    pass
+    print(a.unittest)
